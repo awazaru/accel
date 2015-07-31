cf29dfe0ae082a6b808d64179ff4b2b975d25fb3
awazaru <awazaru@yudaisMBA.local>
2015-07-31 23:12:37 +0900

    インデント

--- firmware/main.c
+++ firmware/main.c
@@ -34,49 +34,49 @@ int8_t x_rad=0,y_rad=0;//角度
 
 /*関数宣言*/
 void usart_tx(unsigned char data){//送信用関数
-    while( !(UCSR0A & (1<<UDRE0)) );        //送信ﾊﾞｯﾌｧ空き待機
-    UDR0 = data;
+  while( !(UCSR0A & (1<<UDRE0)) );        //送信ﾊﾞｯﾌｧ空き待機
+  UDR0 = data;
 }
 
 unsigned char usart_rx(void){//受信用関数
-    while( !(UCSR0A & (1<<RXC0)) );                //受信完了待機
-    return UDR0;                                //受信ﾃﾞｰﾀ返す
+  while( !(UCSR0A & (1<<RXC0)) );                //受信完了待機
+  return UDR0;                                //受信ﾃﾞｰﾀ返す
 }
 
 
 void puts_tx(char *str){//文字列送信用関数
-    while( *str != 0x00 ){ //nullになるまで実行
-        usart_tx(*str);
-        str++;                                    //ｱﾄﾞﾚｽ+1
-    }
+  while( *str != 0x00 ){ //nullになるまで実行
+    usart_tx(*str);
+    str++;                                    //ｱﾄﾞﾚｽ+1
+  }
 }
 
 void serial_ini(){// シリアル通信設定
-    UBRR0 = MYUBRR;
-    UCSR0A=0b00000000;//受信すると10000000 送信有効になると00100000
-    UCSR0B|=0b00011000;//送受信有効
-    UCSR0C=0b00000110;//データ8bit、非同期、バリティなし、stop1bit
+  UBRR0 = MYUBRR;
+  UCSR0A=0b00000000;//受信すると10000000 送信有効になると00100000
+  UCSR0B|=0b00011000;//送受信有効
+  UCSR0C=0b00000110;//データ8bit、非同期、バリティなし、stop1bit
 }
 
 void print_tx(int val)    //PWMのﾃﾞｰﾀ＆電圧値換算し送信
 {
-    if(val<0){
-        val=-val;
-        puts_tx("-");
-    }
-    unsigned char a4,a3,a2,a1;        //PWMのﾃﾞｰﾀ各桁
+  if(val<0){
+    val=-val;
+    puts_tx("-");
+  }
+  unsigned char a4,a3,a2,a1;        //PWMのﾃﾞｰﾀ各桁
     
-    a4 = (val/1000)+0x30;        //1000の位
-    a3 = (val/100)%10+0x30;        //100の位
-    a2 = (val/10)%10+0x30;        //10の位
-    a1 = (val%10)+0x30;            //1の位
+  a4 = (val/1000)+0x30;        //1000の位
+  a3 = (val/100)%10+0x30;        //100の位
+  a2 = (val/10)%10+0x30;        //10の位
+  a1 = (val%10)+0x30;            //1の位
     
-    usart_tx(a4);        //ﾃﾞｰﾀ各桁の送信
-    usart_tx(a3);
-    usart_tx(a2);
-    usart_tx(a1);
+  usart_tx(a4);        //ﾃﾞｰﾀ各桁の送信
+  usart_tx(a3);
+  usart_tx(a2);
+  usart_tx(a1);
     
-    return;
+  return;
 }
 
 void timer_ini(){//タイマー設定
@@ -85,107 +85,100 @@ void timer_ini(){//タイマー設定
 }
 
 void adc_ini(){//ADコンバーター設定
-    //ADUMXはmain文for(;;)内にあります
-    ADCSRA |=_BV(ADEN)|_BV(ADPS1);//ADC許可,割込み許可,4分周 ADSCを1でAD変換開始
-    DIDR0 |=_BV(PIN0)|_BV(PIN1)|_BV(PIN2);//ADC0,1,2ピンデジタル入力禁止
+  //ADUMXはmain文for(;;)内にあります
+  ADCSRA |=_BV(ADEN)|_BV(ADPS1);//ADC許可,割込み許可,4分周 ADSCを1でAD変換開始
+  DIDR0 |=_BV(PIN0)|_BV(PIN1)|_BV(PIN2);//ADC0,1,2ピンデジタル入力禁止
 }
 
 void pin_ini(){
-    DDRC=0;
-    DDRD=0b10000000;//PD7
+  DDRC=0;
+  DDRD=0b10000000;//PD7
 }
 
 void debug(int ad_data){
-    print_tx(ad_data);
-   // _delay_ms(100);
+  print_tx(ad_data);
+  // _delay_ms(100);
 }
 
 int adc_convert(void){//AD変換及びシリアル通信
-    if(i>=4)
-        i=1;
-    int adc_data;
-    ADCSRA|= _BV(ADSC);//ADC開始
-    while(is_SET(ADCSRA,ADIF)==0);  //変換終了まで待機
-    adc_data=(ADCL>>6)+(ADCH<<2);
-    switch (i) {
-        case 1: puts_tx("X:");
-            x_data = adc_data;
-            x_data=x_data-X_G;//オフセット調整
-            break;
-        case 2: puts_tx("Y:");
-            y_data = adc_data;
-            y_data=y_data-Y_G;
-            break;
-        case 3: puts_tx("Z:");
-            z_data = adc_data;
-            z_data=z_data-Z_G;
-            break;
-        default:
-            break;
-    }
-    debug(adc_data);
-    i++;
-    return 0;
+  if(i>=4)
+    i=1;
+  int adc_data;
+  ADCSRA|= _BV(ADSC);//ADC開始
+  while(is_SET(ADCSRA,ADIF)==0);  //変換終了まで待機
+  adc_data=(ADCL>>6)+(ADCH<<2);
+  switch (i) {
+  case 1: puts_tx("X:");
+    x_data = adc_data;
+    x_data=x_data-X_G;//オフセット調整
+    break;
+  case 2: puts_tx("Y:");
+    y_data = adc_data;
+    y_data=y_data-Y_G;
+    break;
+  case 3: puts_tx("Z:");
+    z_data = adc_data;
+    z_data=z_data-Z_G;
+    break;
+  default:
+    break;
+  }
+  debug(adc_data);
+  i++;
+  return 0;
 }
 
 /*メインルーチン*/
 int main(void){
     
-    serial_ini();
-    adc_ini();
-    pin_ini();
+  serial_ini();
+  adc_ini();
+  pin_ini();
     
-    sei();//allow interrupt
-    ADCSRA|= _BV(ADSC);//ADC初期化　単独変換動作
+  sei();//allow interrupt
+  ADCSRA|= _BV(ADSC);//ADC初期化　単独変換動作
     
-    for(;;){
-        ADMUX|=_BV(ADLAR)|_BV(MUX0);//Aref外部基準電圧　ADC1
-        ADMUX&=~_BV(MUX1);
-        adc_convert();//AD変換及び値代入
-        HT;
-        ADMUX|=_BV(ADLAR)|_BV(MUX1); //ADC2
-        ADMUX&=~_BV(MUX0);
-        adc_convert();//AD変換及び値代入
-        HT;
-        ADMUX|=_BV(ADLAR)|_BV(MUX1)|_BV(MUX0);//ADC3
-        adc_convert();//AD変換及び値代入
-        LF;
+  for(;;){
+    ADMUX|=_BV(ADLAR)|_BV(MUX0);//Aref外部基準電圧　ADC1
+    ADMUX&=~_BV(MUX1);
+    adc_convert();//AD変換及び値代入
+    HT;
+    ADMUX|=_BV(ADLAR)|_BV(MUX1); //ADC2
+    ADMUX&=~_BV(MUX0);
+    adc_convert();//AD変換及び値代入
+    HT;
+    ADMUX|=_BV(ADLAR)|_BV(MUX1)|_BV(MUX0);//ADC3
+    adc_convert();//AD変換及び値代入
+    LF;
         
-       /* x_rad=atan2(x_data,z_data)/M_PI*180.0;//角度
-        y_rad=atan2(y_data,z_data)/M_PI*180.0;
-        debug(x_rad);
-        HT;
-        debug(y_rad);
-        LF;
-      */
+    /* x_rad=atan2(x_data,z_data)/M_PI*180.0;//角度
+	  y_rad=atan2(y_data,z_data)/M_PI*180.0;
+	  debug(x_rad);
+	  HT;
+	  debug(y_rad);
+	  LF;
+    */
         
-        //ふりふりしてる間LED点灯
-        xg_data=(x_data*10)/G_unit/9.8;
-        yg_data=(y_data*10)/G_unit/9.8;
-        zg_data=(z_data*10)/G_unit/9.8;
-        xyz_data=sqrt(xg_data*xg_data+yg_data*yg_data+zg_data*zg_data);
-        debug(xyz_data);//加速度を10倍にして表示
-        if(xyz_data>=2.3){
-            shuffle =1;
-            puts_tx("accel");
-        }else{
-            shuffle =0;
-        }
-        LF;
-        
-        if(shuffle==1){
-            PORTD=0b10000000;
-        }else{
-            PORTD=0b00000000;
-        }
-        /*
-        debug(x_data);
-        HT;
-        debug(y_data);
-        HT;
-        debug(z_data);
-        LF;
-        */
+    //ふりふりしてる間LED点灯
+    xg_data=(x_data*10)/G_unit/9.8;
+    yg_data=(y_data*10)/G_unit/9.8;
+    zg_data=(z_data*10)/G_unit/9.8;
+    xyz_data=sqrt(xg_data*xg_data+yg_data*yg_data+zg_data*zg_data);
+    debug(xyz_data);//加速度を10倍にして表示
+    if(xyz_data>=2.3){
+	 shuffle =1;
+	 puts_tx("accel");
+    }else{
+	 shuffle =0;
     }
-    return 0;
+    LF;
+        
+    if(shuffle==1){
+	 PORTD=0b10000000;
+    }else{
+	 PORTD=0b00000000;
+    }
+
+  }
+  return 0;
 }
